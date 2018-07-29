3c728c3aec5695b1eef31fa289dbf3cc94f9893e
summerlove91 <gkdus3040@gmail.com>
2018-07-28 19:05:03 -0700

    search feature added

--- src/components/app.js
+++ src/components/app.js
@@ -12,24 +12,32 @@ export default class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { videos: [],
+                   selectedVideo: null
+     };
+    this.videoSearch('surfboards');
 
-    YTSearch({key: API_KEY, term:'siberian huskies'}, (videos) => {
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       console.log(videos);
-      this.setState({ videos })
-      // this.setState({ videos:videos });
-      // applicable only where key& value pair are same..
+      this.setState({ videos:videos,
+                      selectedVideo: videos[0]
+       });
     });
   }
 
   render() {
     return (
       <div>
-        <div>React simple starter</div>
+        <div>YOUTUBE</div>
         <div>
-          <SearchBar />
-          <VideoDetail video={this.state.videos[0]}/>
-          <VideoList videos={this.state.videos}/>
+          <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+          <VideoDetail video={this.state.selectedVideo}/>
+          <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
         </div>
       </div>
     );
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
   // const video = props.video;
   const imageUrl = video.snippet.thumbnails.default.url;
 
   return (
-    <li className="list-group-item">
+    <li onClick={()=> onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={imageUrl}/>
@@ -13,7 +13,6 @@ const VideoListItem = ({video}) => {
         <div className="media-body">
           <div className="media-heading">{video.snippet.title}</div>
         </div>
-
       </div>
     </li>
   );
